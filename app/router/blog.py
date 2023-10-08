bd4d3e7a659016a195f68bc9cf8cd4083f732ec7
RaghwendraSingh <raghz1994@gmail.com>
2023-10-08 22:25:01 +0530

    added multi value query param, and validation

--- app/router/blog.py
+++ app/router/blog.py
@@ -1,7 +1,7 @@
-from fastapi import APIRouter, Response, status
+from fastapi import APIRouter, Response, status, Path, Query, Body
 from pydantic import BaseModel
 
-from typing import Optional 
+from typing import Optional, List, Dict
 from enum import Enum
 
 router = APIRouter(prefix="/blog", tags=['blog'])
@@ -10,7 +10,7 @@ router = APIRouter(prefix="/blog", tags=['blog'])
 # def all_blogs():
 #     return {"blogs": [{"blog1": "this is blog 1"}, {"blog2": "this is blog 2"}, {"blog3": "this is blog 3"}, {"blog4": "this is blogs 4"}]}
 
-@router.get("//all")
+@router.get("/all")
 def all_blogs(page=1, page_size: Optional[int] = None):
     return {"blog": f"this is blog's page {page} of size {page_size}"}
 
@@ -44,14 +44,22 @@ def get_blog_by_id_comment_id(id: int, comment_id: int, is_valid: bool = True, u
     """
     return {"blog": f"this is blog {id} , with comment {comment_id} - status : {is_valid}, by username: {username}"}
 
+class Image(BaseModel):
+    url: str
+    alias: str
+
 class BlogModel(BaseModel):
     title: str
     type: BlogType
     content: str
+    publisher: Optional[bool]
+    tags: List[str]
+    metadata: Dict[str, str]
+    image: Optional[Image]
 
 
 @router.post("/create", status_code=201, summary='create blog')
-def create_blog(blog: BlogModel):
+def create_blog(blog: BlogModel ):
     """
     API simulation to create a new blog post
     - **blog_name** (query parameter) name of the blog
@@ -59,3 +67,25 @@ def create_blog(blog: BlogModel):
     - **blog_text**
     """
     return {"data": blog}
+
+@router.post("/new/{id}/comment/{comment_id}")
+def create_comment(blog: BlogModel, id: int, 
+                   comment_title: str = Query(
+                       None, title="Id of the comment", 
+                       description="Some description for comment id",
+                       alias='commentTitle',
+                       deprecated=True),
+                    content: str = Body(...,
+                                   min_length=0,
+                                   max_length=50,
+                                   regex='^[a-z\s]*$'),
+                    version: Optional[List[str]] = Query(['1.0', '1.1', '1.2']),
+                    comment_id: int = Path(None, gt=5, le=10)):
+    return {
+        'blog': blog,
+        'id': id,
+        'comment_title': comment_title,
+        'content': content,
+        'comment_id': comment_id,
+        'version': version
+    }
