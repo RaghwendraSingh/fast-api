a834e9772fd1d4322f0e4298308233b4a677262e
RaghwendraSingh <raghz1994@gmail.com>
2023-10-23 03:00:46 +0530

    added router for read all & read one user

--- app/router/user.py
+++ app/router/user.py
@@ -1,9 +1,11 @@
-from fastapi import APIRouter, Response, status, Depends
-from sqlalchemy.orm.session import Session
+from fastapi import APIRouter, Response, status, Depends, Body
+from sqlalchemy.orm import Session
 
 from db.database import get_db
 from db.schemas import UserBase, UserDisplay
 from db import db_user
+
+from typing import List
 
 router = APIRouter(prefix="/user", tags=["user"])
 
@@ -13,6 +15,13 @@ def create_user(user: UserBase, db: Session = Depends(get_db)) -> UserDisplay:
     return db_user.create_user(db, user)
 
 # Read user
+@router.get("/", response_model=List[UserDisplay])
+def get_all_users(db: Session = Depends(get_db)):
+    return db_user.get_all_users(db)
+
+@router.get("/{id}", response_model=UserDisplay)
+def get_user(id: int, db: Session = Depends(get_db)):
+    return db_user.get_user(db, id)
 
 # update user
 
