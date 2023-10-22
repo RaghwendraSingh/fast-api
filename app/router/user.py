7140c3fdd87e607ec4b9979e64a7ba33036a8c82
RaghwendraSingh <raghz1994@gmail.com>
2023-10-23 02:24:59 +0530

    changed the Users router response_model to UserDisplay schema

--- app/router/user.py
+++ app/router/user.py
@@ -2,14 +2,14 @@ from fastapi import APIRouter, Response, status, Depends
 from sqlalchemy.orm.session import Session
 
 from db.database import get_db
-from db.schemas import UserBase
+from db.schemas import UserBase, UserDisplay
 from db import db_user
 
 router = APIRouter(prefix="/user", tags=["user"])
 
 # create user
-@router.post('/')
-def create_user(user: UserBase, db: Session = Depends(get_db)):
+@router.post('/', response_model=UserDisplay)
+def create_user(user: UserBase, db: Session = Depends(get_db)) -> UserDisplay:
     return db_user.create_user(db, user)
 
 # Read user
